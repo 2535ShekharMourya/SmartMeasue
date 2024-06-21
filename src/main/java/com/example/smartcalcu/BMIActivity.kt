e89b3cc204045178606c8137bfa6e6a6dce075eb
Azadshekhar <shekharmaurya8978@gmail.com>
2024-06-21 10:32:12 +0530

    second commit

--- src/main/java/com/example/smartcalcu/BMIActivity.kt
+++ src/main/java/com/example/smartcalcu/BMIActivity.kt
@@ -1,10 +1,14 @@
 package com.example.smartcalcu
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import com.example.smartcalcu.databinding.ActivityBmiactivityBinding
+import java.util.Locale
 
-class BMIActivity : AppCompatActivity() {
+class BMIActivity : AppCompatActivity(),TextToSpeech.OnInitListener {
+    lateinit var tts: TextToSpeech
     lateinit var binding: ActivityBmiactivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +16,7 @@ class BMIActivity : AppCompatActivity() {
         binding = ActivityBmiactivityBinding.inflate(layoutInflater)
         // Set the content view to the root view of the binding
         setContentView(binding.root)
+        tts = TextToSpeech(this, this)
         binding.weightPicker.minValue = 30
         binding.weightPicker.maxValue = 150
 
@@ -44,13 +49,50 @@ class BMIActivity : AppCompatActivity() {
 
     private fun healthyMessage(bmi: Double): String
     {
-        if (bmi < 18.5)
+        if (bmi < 18.5){
+            speakOut("Underweight")
+            binding.layout.setBackgroundColor(Color.parseColor("#Ffff00"))
             return "Underweight"
-        if(bmi < 25.0)
-            return "Healthy"
-        if (bmi < 30.0)
+        }
+        if(bmi < 25.0){
+            binding.layout.setBackgroundColor(Color.parseColor("#008000"))
+        speakOut("Healthy")
+            return "Healthy"}
+        if (bmi < 30.0) {
+            binding.layout.setBackgroundColor(Color.parseColor("#FF0000"))
+            speakOut("Overweight")
             return "Overweight"
-
+        }
+        binding.layout.setBackgroundColor(Color.parseColor("#87CEEB"))
+        speakOut("Obese")
         return "Obese"
+    }
+    fun speakOut(text: String) {
+        tts.speak(text, TextToSpeech.QUEUE_ADD, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Set the language
+            val result = tts.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                // Handle the error
+            } else {
+                // TTS is ready to use
+                speakOut("Welcome to Smart Measure App")
+            }
+        } else {
+            // Initialization failed
+        }
+
+
+    }
+    override fun onDestroy() {
+        // Shutdown TTS when activity is destroyed
+        if (tts.isSpeaking) {
+            tts.stop()
+        }
+        tts.shutdown()
+        super.onDestroy()
     }
 }
